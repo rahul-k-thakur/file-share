befc427fb1cfd8c36d647d211200289929ddff36
Rahul Kumar <rahul.k.thakur@outlook.com>
2023-07-19 09:08:11 +0530

    app changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-// import styles from './App.module.scss';
-
+import classNames from 'classnames';
+import styles from './App.module.scss';
 import { Card } from "./components/card/card";
 
 function App() {
     return (
         <>
-            <Card fileId={1} fileName="Lord of the Rings.mp4" fileMimeType="ogg" fileSize={1024*1024*700} />
+            <div className={classNames(styles.App)}>
+                <div className={classNames(styles.Files)} >
+                    <Card fileId={1} fileName="Lord of the Rings.mp4" fileMimeType="ogg" fileSize={1024*1024*700} />
+                </div>
+                <div className={classNames(styles.DropJone)} >
+                    Hello
+                </div>
+            </div>
         </>
     );
 }
